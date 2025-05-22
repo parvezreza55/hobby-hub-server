bc26d94031ec44423d0edbc08eacb481d9079095
parvez <parvezreza112@gmail.com>
2025-05-23 01:43:18 +0600

    added user api

--- index.js
+++ index.js
@@ -22,20 +22,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const usersCollection = client.db("hobbyUser").collection("users");
+
+    app.post("/user", async (req, res) => {
+      const users = req.body;
+      const result = await usersCollection.insertOne(users);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
-
-app.get("/", (req, res) => {
-  res.send("app is running");
+app.get("/user", async (req, res) => {
+  res.send("user coming");
 });
 app.listen(port, () => {
   console.log(`app is running in the port ${port}`);
